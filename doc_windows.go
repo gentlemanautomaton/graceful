689b1a597da66ed7e202501ee158e7ed04268cb0
Joshua Sjoding <ga@josh.hey.nu>
2017-07-27 06:48:27 -0700

    Minor edits to documentation

--- doc_windows.go
+++ doc_windows.go
@@ -5,10 +5,10 @@
 // calls ExitProcess() from within the target process' address space.
 //
 // This package is based on ideas put forth by Andrew Tucker in a Dr. Dobb's
-// article published in 1999:
+// article published in 1999 titled "A Safer Alternative to TerminateProcess()":
 // http://www.drdobbs.com/a-safer-alternative-to-terminateprocess/184416547
 //
-// This package also draws inspiration from some of the edge cases covered
-// by the git-for-windows implementation of the same idea:
+// This package also draws inspiration the git-for-windows implementation of
+// the same idea:
 // https://github.com/git-for-windows/msys2-runtime/pull/15/commits/e9cb332976cf6ba44d9f5fc0ed4f725ce43fe646
 package graceful
